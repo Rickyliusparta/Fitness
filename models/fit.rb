21af9f8aa3c1ef0fe6cd773df163ca3dd134c8c9
Ricky Liu <rliu@spartaglobal.co>
2017-06-09 11:20:31 +0100

    starting to ceate table

--- models/fit.rb
+++ models/fit.rb
@@ -4,5 +4,31 @@ class Fit
    PG.connect(dname: "forum")
   end
 
+  def self.hydrate fit_data
+
+    fit = Fit.new
+    fit.id = fit_data['id']
+    fit.first_name = fit_data['first_name']
+    fit.age = fit_data['age']
+    fit.gender = fit_data['gender']
+    fit.experience = fit_data['experience']
+    fit.title = fit_data['title']
+    fit.body = fit_data['body']
+
+  end
+
+  # INDEX
+  def self.all
+    conn = self.open_connection
+    sql = "SELECT id,first_name,age,gender,experience,date,title,body FROM post ORDER BY id"
+    results = conn.exec(sql)
+
+        # create an array of post objects
+    fits = results.map do |tuple| 
+      self.hydrate tuple
+    end
+  end
+
+  
  
 end
--- seed.sql
+++ seed.sql
@@ -3,13 +3,17 @@ DROP TABLE IF EXISTS fitness;
 CREATE TABLE fitness (
 
   id SERIAL PRIMARY KEY,
-  title VARCHAR(255),
-  body TEXT
+  first_name VARCHAR(25),
+  age	 NUMERIC,
+  gender VARCHAR(7),
+  experience  TEXT, 
+  date	 DATE,
+  title  VARCHAR(50),
+  body  TEXT
 
 );
 
-INSERT INTO fitness (first_name, age, experience, date,  title , body, ) VALUES ('first name', 'age', 'date', 'experience','fitness 1' , 'Body text 1');
-INSERT INTO fitness (first_name, age, experience, date,  title , body, ) VALUES ('first name', 'age', 'date', 'experience','fitness 1' , 'Body text 2');
-INSERT INTO fitness (first_name, age, experience, date,  title , body, ) VALUES ('first name', 'age', 'date', 'experience','fitness 1' , 'Body text 3');
-INSERT INTO fitness (first_name, age, experience, date,  title , body, ) VALUES ('first name', 'age', 'date', 'experience','fitness 1' , 'Body text 4');
-INSERT INTO fitness (first_name, age, experience, date,  title , body, ) VALUES ('first name', 'age', 'date', 'experience','fitness 1' , 'Body text 5');
+INSERT INTO fitness (first_name, age, gender, experience, date,  title , body, ) VALUES ('first name', 'age', 'date', 'gender', 'experience','fitness 1' , 'Body text 1');
+INSERT INTO fitness (first_name, age, gender, experience, date,  title , body, ) VALUES ('first name', 'age', 'date', 'gender', 'experience','fitness 2' , 'Body text 2');
+INSERT INTO fitness (first_name, age, gender, experience, date,  title , body, ) VALUES ('first name', 'age', 'date', 'gender', 'experience','fitness 3' , 'Body text 3');
+INSERT INTO fitness (first_name, age, gender, experience, date,  title , body, ) VALUES ('first name', 'age', 'date', 'gender', 'experience','fitness 4' , 'Body text 4');
