2c839776fcea7770559da37107ccdcb23f9d50d7
Ricky Liu <rliu@spartaglobal.co>
2017-06-12 20:36:57 +0100

    new table added, changed experience to numeric

--- seed.sql
+++ seed.sql
@@ -6,18 +6,13 @@ CREATE TABLE fits (
   first_name VARCHAR(25) NULL,
   age	 NUMERIC NULL,
   gender VARCHAR(7) NULL,
-  experience  TEXT NULL, 
+  experience  NUMERIC NULL, 
   date	 DATE NULL,
   title  VARCHAR(50) NULL,
   body  TEXT NULL,
-  file VARCHAR(100) NULL,
-  postcode TEXT NULL,
-  longitude FLOAT NULL,
-  latitude FLOAT NULL
-
-
-
+  file VARCHAR(100) NULL
 );
+
 
 INSERT INTO fits (first_name, age, gender, experience, date, title, body) VALUES ('Ricky' , '27' , 'male' , '1 year','09/05/2017', 'diet' , 'how to eat healthy');
 INSERT INTO fits (first_name, age, gender, experience, date,  title , body) VALUES ('first name', 'age' , 'gender' , 'experience', 'date' , 'title' , 'Body text 2');
