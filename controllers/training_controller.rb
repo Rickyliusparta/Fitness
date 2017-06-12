249bf2cde78b5b5be077704bed3a03d08170071c
Ricky Liu <rliu@spartaglobal.co>
2017-06-12 15:39:54 +0100

    2nd controller built

--- controllers/training_controller.rb
+++ controllers/training_controller.rb
@@ -5,7 +5,7 @@ class TrainingsController < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
   
   # sets the view directory correctly
-  set :views, Proc.new { File.join(root, "views") } 
+  set :views, Proc.new { File.join(root, "views/training") } 
 
   configure :development do
       register Sinatra::Reloader
@@ -20,5 +20,13 @@ class TrainingsController < Sinatra::Base
     erb :'trainings/index'
   end
 
+  get "/:id" do
+
+    id - params[:id].to_i
+
+    @fitness = Fit.find id
+
+    erb :'trainings/show'
+  end
 end
 
